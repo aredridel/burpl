1f23984ca7de36e2b2e500e5a264bcb065654524
Aria Stewart <aredridel@nbtsc.org>
2017-11-25 11:23:46 -0500

    rlwip

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { promisify } = require('util')
 const connect = require('net').connect
 const rpcs = require('multiplex-rpc')
 const pump = promisify(require('pump'))
+const readline = require('readline')
 
 const args = require('yargs')
 	.usage('$0 -s socket')
@@ -18,11 +19,33 @@ async function main(args) {
 	const o = rpc.wrap(['command', 'list'])
 	const command = promisify(o.command)
 	const list = promisify(o.list)
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		completer: (line, cb) => {
+			try {
+				const completions = await list(line)
+				cb(null, [completions, line])
+			} catch (e) {
+				cb(e)
+			}
+		}
+	})
+
+	rl.on('line', async line => {
+		console.log(line)
+		console.log(await command(line))
+		rl.prompt()
+	})
+
+	rl.on('error', err => console.error(err))
+
+	rl.prompt()
 
 	const conn = pump(rpc, sock, rpc)
 
-	console.warn(await list())
-	console.warn(await command('user list', []))
+	//console.warn(await list())
+	//console.warn(await command('user list', []))
 	rpc.end()
 	await conn
 }
