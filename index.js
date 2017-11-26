49367380b50cfe5a8bce29592314b60e94cfa8e2
Aria Stewart <aredridel@dinhe.net>
2017-11-26 01:12:49 -0500

    almost

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const connect = require('net').connect
 const rpcs = require('multiplex-rpc')
 const pump = promisify(require('pump'))
 const readline = require('readline')
+const commonPrefix = require('common-prefix')
 
 const args = require('yargs')
 	.usage('$0 -s socket')
@@ -22,11 +23,52 @@ async function main(args) {
 	const rl = readline.createInterface({
 		input: process.stdin,
 		output: process.stdout,
-		completer: (line, cb) => {
+		completer: async (line, cb) => {
 			try {
 				const completions = await list(line)
-				cb(null, [completions, line])
+				const trie = compl2trie(completions)
+				const words = line.split(/\s+/)
+
+				let n = trie
+				const prefix = []
+
+				// for each word, if the word is unambiguous, expand it
+				// if the word is ambiguous and we're at the end, expand as much as we can, else stop
+				for (var i = 0; i < words.length; i++) {
+					if (!n) break
+					const w = words[i];
+					const pattern = n.children.find(w => w.key == '_')
+					const x = n.children.filter(k => k.key.startsWith(w))
+					if (pattern) {
+						console.warn('__')
+						prefix.push(w)
+						n = pattern
+					} else if (x.length == 1) {
+						console.warn('ex')
+						prefix.push(x[0].key)
+						n = x[0]
+					} else if (i + 1 == words.length) {
+						console.warn('cp')
+						const cp = commonPrefix(n.children.map(e => e.key).filter(e => e.startsWith(w)))
+						prefix.push(cp)
+						break
+					} else {
+						break
+					}
+				}
+
+				console.warn('i', i, 'words.length', words.length, 'n', n)
+
+				if (i == words.length || words[words.length - 1] == '' && n) {
+					// find matching suffixes
+					const suffixes = trie2list(n, prefix).map(e => e.join(' '))
+					console.warn(suffixes)
+					return cb(null, [suffixes, line])
+				} else {
+					return cb(null, [[prefix.join(' ')], line])
+				}
 			} catch (e) {
+				console.warn(e)
 				cb(e)
 			}
 		}
@@ -46,6 +88,42 @@ async function main(args) {
 
 	//console.warn(await list())
 	//console.warn(await command('user list', []))
-	rpc.end()
+	rl.on('close', () => rpc.end())
+		
 	await conn
+}
+
+function isUpper(word) {
+	return /^[Amap(match).-Z]+$/.test(word) 
+}
+
+function compl2trie(completions) {
+	const root = { children: [] }
+	completions.forEach(e => {
+		let n = root
+		e.split(/\s+/).forEach(w => {
+			const k = isUpper(n.children[w]) ? '_' : w
+			if (!n.children.find(e => e.key == k)) {
+				const c = { key: k, children: [] }
+				n.children.push(c)
+				n = c
+			} else {
+				n = n.children.find(e => e.key == k)
+			}
+		})
+	})
+
+	return root
+}
+
+function trie2list(n, pre) {
+	if (!n.children.length) return [pre]
+
+	let out = []
+
+	n.children.forEach(e => {
+		out = out.concat(trie2list(e, pre.concat(e.key)))
+	})
+
+	return out
 }
