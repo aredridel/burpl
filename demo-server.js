f1a689c75a5a987282d27afff7e2d1a41ede6cd3
Aria Stewart <aredridel@nbtsc.org>
2017-12-01 01:52:24 -0500

    Extract server side into a component that can be reused

--- demo-server.js
+++ demo-server.js
@@ -1,30 +1,11 @@
-const { createServer } = require('net')
-const { promisify, callbackify } = require('util')
+const burpl = require('./')
+const { promisify } = require('util')
 const listen = promisify(require('unix-listen'))
-const rpcs = require('multiplex-rpc')
 
-const server = createServer(async conn => {
-    const commands = [
-        "user list",
-        "user add USER",
-        "user add USER test"
-    ]
-    const rpc = rpcs({
-        list: callbackify(async () => {
-            return commands
-        }),
-        command: callbackify(async (name, args) => {
-            if (commands.indexOf(name) == -1) throw new Error(`Unknown command '${name}'`)
-            console.warn(name, args)
-        })
-    })
-
-    try {
-        conn.pipe(rpc).pipe(conn)
-        //await pump(rpc, conn, rpc)
-    } catch (e) {
-        console.error(e.stack || e)
-    }
+const server = burpl({
+    "user list": () => { },
+    "user add USER": () => { },
+    "user add USER test": () => {}
 })
 
 listen(server, 'test.sock')
