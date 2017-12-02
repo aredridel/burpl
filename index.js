879dec1ae07b30c9eb9edf00c9f601a92ff26dc4
Aria Stewart <aredridel@nbtsc.org>
2017-12-02 01:41:47 -0500

    The index.js file. Sigh.

---
+++ index.js
@@ -0,0 +1,31 @@
+const { createServer } = require('net')
+const { callbackify } = require('util')
+const rpcs = require('multiplex-rpc')
+
+
+module.exports = function(language) {
+
+    const server = createServer(async conn => {
+        const commands = Object.keys(language)
+
+        const rpc = rpcs({
+            list: callbackify(async () => {
+                return commands
+            }),
+            command: callbackify(async (name, args) => {
+                if (!language[name]) throw new Error(`Unknown command '${name}'`)
+                return language[name].apply(null, args)
+            })
+        })
+
+        try {
+            conn.pipe(rpc).pipe(conn)
+            //await pump(rpc, conn, rpc)
+        } catch (e) {
+            console.error(e.stack || e)
+        }
+    })
+
+    return server
+
+}
