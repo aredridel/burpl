393943f54bb19fd7b88f138101d6631f078a9110
Aria Stewart <aredridel@nbtsc.org>
2017-12-03 00:14:56 -0500

    Use minimist because our needs are so small

--- cli.js
+++ cli.js
@@ -1,73 +1,88 @@
 #!/usr/bin/env node
-const { promisify } = require('util')
+const {promisify} = require('util')
 const connect = require('net').connect
 const rpcs = require('multiplex-rpc')
 const pump = promisify(require('pump'))
 const readline = require('readline-ext')
+const minimist = require('minimist')
 
 const match = require('./match')
 
-const args = require('yargs')
-	.usage('$0 -s socket')
-	.alias('s', 'socket')
-	.describe('s', 'path to socket to connect to')
-	.demandOption('s')
-	.argv
+const args = minimist(process.argv.slice(2), {
+  string: ["s", "socket"],
+  boolean: ["help"],
+  unknown: opt => {
+    console.warn(`Unknown option ${opt}`)
+    process.exit(1)
+  }
+})
 
-main(args).catch(err => {
-	console.warn(err.code ? err.message : err)
-	process.exit(1)
+const socket = args.socket || args.s
+
+if (args.help || !socket) {
+  console.warn(`Usage: burpl -s socket`);
+  process.exit(0)
+}
+
+main({
+  socket
+}).catch(err => {
+  console.warn(err.code ? err.message : err)
+  process.exit(1)
 })
 
 async function main(args) {
-	const sock = await connect(args.socket)
-	const rpc = rpcs()
-	const o = rpc.wrap(['command', 'list'])
-	const command = promisify(o.command)
-	const list = promisify(o.list)
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-		completer: async (line, cb) => {
-			try {
-				const completions = await list(line)
-				const matched = match(completions, line)
-				return cb(null, { completions: matched.completions, replace: true })
-			} catch (e) {
-				console.warn(e)
-				cb(e)
-			}
-		}
-	})
+  const sock = await connect(args.socket)
+  const rpc = rpcs()
+  const o = rpc.wrap(['command', 'list'])
+  const command = promisify(o.command)
+  const list = promisify(o.list)
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    completer: async (line, cb) => {
+      try {
+        const completions = await list(line)
+        const matched = match(completions, line)
+        return cb(null, {
+          completions: matched.completions,
+          replace: true
+        })
+      } catch (e) {
+        console.warn(e)
+        cb(e)
+      }
+    }
+  })
 
-	rl.on('line', async line => {
-		line = line.trim()
-		const completions = await list(line)
-		const matched = match(completions, line)
+  rl.on('line', async line => {
+    line = line.trim()
+    const completions = await list(line)
+    const matched = match(completions, line)
 
-		try {
-			if (line) {
-				if (!matched.command) {
-					console.warn(`No such command: ${line}`)
-				} else {
-					const response = await command(matched.command, matched.args)
-					if (response) console.log(response)
-				}
-			}
-		} catch (e) {
-			console.warn(e)
-		}
-		rl.prompt()
-	})
+    try {
+      if (line) {
+        if (!matched.command) {
+          console.warn(`No such command: ${line}`)
+        } else {
+          const response = await command(matched.command, matched.args)
+          if (response) console.log(response)
+        }
+      }
+    } catch (e) {
+      console.warn(e)
+    }
+    rl.prompt()
+  })
 
-	rl.on('error', err => console.error(err))
+  rl.on('error', err => console.error(err))
 
-	rl.prompt()
+  rl.prompt()
 
-	const conn = pump(rpc, sock, rpc)
+  const conn = pump(rpc, sock, rpc)
 
-	sock.on('end', () => rl.close())
-	rl.on('close', () => rpc.end())
-		
-	await conn
+  sock.on('end', () => rl.close())
+  rl.on('close', () => rpc.end())
+
+  await conn
 }
--- demo-server.js
+++ demo-server.js
@@ -1,11 +1,20 @@
-const burpl = require('./')
+#!/usr/bin/env node
+const burpl = require('.')
 const { promisify } = require('util')
 const listen = promisify(require('unix-listen'))
 
+const users = [ "aphrodite", "eris", "persephone", "sappho"];
+
 const server = burpl({
-    "user list": () => ["kat", "katrina", "katriona"].join("\n"),
-    "user add USER": (user) => `added user '${user}'`,
-    "user add USER test": (user) =>`added user '${user}' in test mode`,
+    "help": async () => "There is no help for you",
+    "user list": () => users.join("\n"),
+    "user add USER": (user) => {
+		return `added user '${user}' (I lie)`;
+	},
+    "user add USER really": async (user) => {
+		await Promise.resolve(user).then(user => users.push(user));
+		return `added user '${user}' for real`;
+	},
 })
 
 listen(server, 'test.sock')
